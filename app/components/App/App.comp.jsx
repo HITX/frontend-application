50b9e643a2b0be96a81b668d7534af4cb700fa4f
Ben Parham <ben@playlist.com>
2015-08-09 10:36:39 -0700

    Add react skeleton along with gulp and preliminary build process

---
+++ app/components/App/App.comp.jsx
@@ -0,0 +1,11 @@
+'use strict'
+
+var React = require('react');
+
+var App = React.createClass({
+  render: function() {
+    return (<p>Hello React</p>);
+  }
+});
+
+module.exports = App;
---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+'use strict'
+
+var gulp = require('gulp')
+var browserify = require('browserify');
+var reactify = require('reactify');
+var envify = require('envify');
+var source = require('vinyl-source-stream');
+var rename = require('gulp-rename');
+
+var builder = function builder() {
+  return browserify({
+    entries: './app/main.jsx',
+    extensions: ['.jsx']
+  })
+  .transform(reactify)
+  .transform(envify)
+  .bundle()
+  .pipe(source('main.jsx'))
+  .pipe(rename('bundle.js'));
+};
+
+gulp.task('build-js', function() {
+  return builder()
+    .pipe(gulp.dest('./public/js/'));
+});
+
+// gulp.task('build', ['build-js', 'less']);
+gulp.task('build', ['build-js']);
+
+// gulp.task('watch', ['build', 'run-server', 'less'], function() {
+//   gulp.watch(paths.app, ['build']);
+//   gulp.watch(paths.js, ['build']);
+//   gulp.watch(paths.css, ['less']);
+// });
+
+// gulp.task('default', ['watch']);
+gulp.task('default', ['build']);
