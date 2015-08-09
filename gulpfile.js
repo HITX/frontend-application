09d65601a8ae0c4fd41ca66c121208d1ae954a98
Ben Parham <ben@playlist.com>
2015-08-09 11:34:14 -0700

    Add less to build process

--- gulpfile.js
+++ gulpfile.js
@@ -6,17 +6,18 @@ var reactify = require('reactify');
 var envify = require('envify');
 var source = require('vinyl-source-stream');
 var rename = require('gulp-rename');
+var less = require('gulp-less');
 
 var builder = function builder() {
   return browserify({
     entries: './app/main.jsx',
     extensions: ['.jsx']
   })
-  .transform(reactify)
-  .transform(envify)
-  .bundle()
-  .pipe(source('main.jsx'))
-  .pipe(rename('bundle.js'));
+    .transform(reactify)
+    .transform(envify)
+    .bundle()
+    .pipe(source('main.jsx'))
+    .pipe(rename('bundle.js'));
 };
 
 gulp.task('build-js', function() {
@@ -24,8 +25,14 @@ gulp.task('build-js', function() {
     .pipe(gulp.dest('./public/js/'));
 });
 
-// gulp.task('build', ['build-js', 'less']);
-gulp.task('build', ['build-js']);
+gulp.task('less', function() {
+  return gulp.src('./assets/css/styles.less')
+    .pipe(less())
+    .pipe(gulp.dest('./public/css/'));
+});
+
+gulp.task('build', ['build-js', 'less']);
+// gulp.task('build', ['build-js']);
 
 // gulp.task('watch', ['build', 'run-server', 'less'], function() {
 //   gulp.watch(paths.app, ['build']);
